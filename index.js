4c74747844e5d3ffcc9b0caa85a6594ff81dd356
adarsh-gupta101 <adarshgupta2468@gmail.com>
2023-08-19 20:35:49 +0530

    intial blob

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { gsap } from "gsap";
 
 // Debug
 const gui = new dat.GUI();
+// show gui
+gui.hide();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -23,7 +27,7 @@ material.transparent = true;
 
 // Mesh
 const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+// scene.add(sphere);
 
 // Lights
 
@@ -66,19 +70,63 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.x = 0;
-camera.position.y = 0;
+camera.position.y = 4;
 camera.position.z = 2;
 scene.add(camera);
+// dat gui camera
 
+
+
+const loader = new GLTFLoader();
+
+// Load the glTF model
+loader.load(
+  "/iphone_14_pro/iphone_14_pro.glb",
+  (glb) => {
+    const model = glb.scene;
+    // model.scale.set(40, 40, 40);
+    scene.add(model);
+    model.position.y = -1.5;
+    model.position.x = 0;
+    model.position.z = 0;
+    // model.scale.set(0.3, 0.3, 0.3);
+    model.rotation.y = 3.2;
+    // model.rotation.x = 5;
+    // model.rotation.z =0;
+
+    // light
+    const light = new THREE.AmbientLight(0xffffff, 1);
+    scene.add(light);
+
+
+
+    const tl = gsap.timeline();
+    tl.to(model.position, { duration: 1, x:4,y: -14,z:-10, ease: "power2.out" })
+      .to(model.scale, { duration: 1, x: 1, y: 1, z: 1, ease: "power2.out" }, "+=5")
+      .to(model.rotation, { duration: 1, y: 0, ease: "power2.out" }, "-=1")
+      .call(() => {
+        // Animation complete
+        console.log("Animation finished");
+      });
+
+    // Add the loaded model to the scene
+  },
+  undefined,
+  (error) => {
+    console.error(error);
+  }
+);
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
+
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  // alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -108,39 +156,3 @@ const tick = () => {
 };
 
 tick();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
